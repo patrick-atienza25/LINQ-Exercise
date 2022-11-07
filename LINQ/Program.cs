81b4e12c5101d1df0d4f0c6ec4c4c182c7f11ead
patrick atienza <patienza25@gmail.com>
2022-11-07 13:58:12 -0800

    finished exercise 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> games = new List<string>() { "Super Mario Bros. 3", "League of Legends", "Batman Arkham Asylum", "Pokemon Alpha Sapphire" };
+            var gamesInOrder = games.OrderBy(x => x.Length);
+            foreach (var game in gamesInOrder)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
